8640f1a42d905323633ee5a1330ec50dcec30761
Lyunvy <64697357+Lyunvy@users.noreply.github.com>
2022-08-03 15:27:41 +0800

    fix

--- src/footnotes.js
+++ src/footnotes.js
@@ -56,7 +56,7 @@ function renderFootnotes(text) {
             return '<sup id="fnref:' + index + '">' +
                 '<a href="#fn:'+ index +'" rel="footnote">' +
                 '<span class="hint--medium" aria-label="'
-                + tooltip +
+                + md.renderInline(tooltip) +
                 '">[' + index +']</span></a></sup>';
         });
 
