116fc41b12a1230569d5979020c212ae223481c4
LyunvyA <110519315+LyunvyA@users.noreply.github.com>
2022-11-08 21:05:29 +0800

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ hexo.extend.filter.register('before_post_render', function(data) {
 // Add CDN CSS resources
 hexo.extend.filter.register('after_post_render', function(data) {
   data.content =
-      util.htmlTag('link', {rel: 'stylesheet', type: 'text/css', href: './css/web/hint.min.css'}) +
+      util.htmlTag('link', {rel: 'stylesheet', type: 'text/css', href: '/css/web/hint.min.css'}) +
       data.content;
   return data;
 });
